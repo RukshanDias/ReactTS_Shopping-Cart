c38048e056704f9abfcc64738e8839cd924dbce1
RukshanDias <rukshandias002@gmail.com>
2023-06-29 12:24:23 +0530

    currency formating for price

---
+++ src/utilities/formatCurrency.ts
@@ -0,0 +1,9 @@
+// formatting the currency to display price
+const CURRENCY_FORMATTER = new Intl.NumberFormat(undefined, {
+    currency: "USD",
+    style: "currency",
+});
+
+export function formatCurrency(number: number) {
+    return CURRENCY_FORMATTER.format(number);
+}
